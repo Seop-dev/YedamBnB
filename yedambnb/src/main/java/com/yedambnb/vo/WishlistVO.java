a786f372d59439371a1c478cc676d7a5812cc0c9
Your Name <pc@pc.com>
2025-06-30 11:44:49 +0900

    LOGIN TEST

--- yedambnb/src/main/java/com/yedambnb/vo/WishlistVO.java
+++ yedambnb/src/main/java/com/yedambnb/vo/WishlistVO.java
@@ -11,6 +11,5 @@ public class WishlistVO {
 	public Date createdAt;
 	public String name;           // 숙소 이름
 	public int pricePerNight;     // 1박당 가격
-	public String photoPath;   
-	// 대표 이미지 경로
+	public String photoPath;   // 대표 이미지 경로
 }
